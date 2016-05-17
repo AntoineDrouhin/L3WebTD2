3f9600af01b30e22af6b861b12fb3eb71ebf75f7
Aurelien Garret <aurelien.garret@gmail.com>
2016-05-17 14:26:55 +0200

    Controller + bower install

--- web/app/controlers/myResumeCtrl.js
+++ web/app/controlers/myResumeCtrl.js
@@ -3,6 +3,8 @@
 angular.module('myResumeApp')
     .controller('myResumeCtrl', function($scope, $http){
 
-        console.log('heeeeeere')
+        $http.get("./data/my-resume.json").then(function (response) {
+            $scope.cv = response.data;
+        });
 
     });

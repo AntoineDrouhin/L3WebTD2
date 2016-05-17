2cf4e4bdd8fb856132d43e5e8b8f42f86f3de309
Aurelien Garret <aurelien.garret@gmail.com>
2016-05-17 15:34:27 +0200

    Dynamic refresh of cvs

--- web/app/controlers/myResumeCtrl.js
+++ web/app/controlers/myResumeCtrl.js
@@ -4,7 +4,15 @@ angular.module('myResumeApp')
     .controller('myResumeCtrl', function($scope, $http){
 
         $http.get("./data/my-resume.json").then(function (response) {
-            $scope.cv = response.data;
+            $scope.cvs = response.data;
+            if ($scope.cvs.length > 0)
+                $scope.selected = $scope.cvs[0].person.id;
+            else
+                alert("Aucun cv disponible");
         });
+
+        $scope.updateCv =function(id) {
+            $scope.selected = id;
+        };
 
     });
